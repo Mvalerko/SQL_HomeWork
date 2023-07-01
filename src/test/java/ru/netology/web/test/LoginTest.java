555ae0e55d8fbe5b94d8c8d7a631bc275da0bf21
mvalerko <04445326@mail.ru>
2023-07-01 17:08:23 +0300

    Добавлено изменение в тест. Теперь он проходит фоном, не открывая браузер

--- src/test/java/ru/netology/web/test/LoginTest.java
+++ src/test/java/ru/netology/web/test/LoginTest.java
@@ -1,6 +1,8 @@
 package ru.netology.web.test;
 
+import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import ru.netology.web.data.DataHelper;
 import ru.netology.web.data.SQLHelper;
@@ -10,6 +12,7 @@ import static com.codeborne.selenide.Selenide.open;
 import static ru.netology.web.data.SQLHelper.cleanDatabase;
 
 class LoginTest {
+
     @AfterAll
     static void teardown(){
         cleanDatabase();
@@ -17,6 +20,7 @@ class LoginTest {
 
     @Test
     void transferMoneyUnitsTest() {
+        Configuration.headless = true;
         open("http://localhost:9999");
         var loginPage = new LoginPageV2();
         var authInfo = DataHelper.getAuthInfo();
